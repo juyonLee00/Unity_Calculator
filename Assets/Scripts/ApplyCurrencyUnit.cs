dc60093b599dfcba87d226918a05e32549d9dbcc
juyonLee00 <ljy012900@gmail.com>
2023-09-21 22:11:57 +0900

    [chore] 주석 추가

--- Assets/Scripts/ApplyCurrencyUnit.cs
+++ Assets/Scripts/ApplyCurrencyUnit.cs
@@ -1,19 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ApplyCurrencyUnit : MonoBehaviour
 {
-    //gameObject에서 해당 텍스트값 가져와서 해당 텍스트를 뒤에서부터 세 자리일때마다 잘라서 출력.
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    string str;
+
+    public static void split3units()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //문자 3개씩 기준으로 나누기
 }
